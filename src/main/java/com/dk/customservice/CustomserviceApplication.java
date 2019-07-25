4d909db67232b2cce9532ec7594b83dee9640c4c
dkudale <33438062+dkudale@users.noreply.github.com>
2019-07-25 00:20:11 -0400

    Updated

--- src/main/java/com/dk/customservice/CustomserviceApplication.java
+++ src/main/java/com/dk/customservice/CustomserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages= {"com.example.customservice"})
+@ComponentScan(basePackages= {"com.dk.customservice"})
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CustomserviceApplication {
